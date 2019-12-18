fd2416af3c00ab7f245eb10a0a455c2b08ad668d
Daniele Perrella <ktm17@live.it>
2019-12-18 20:06:03 +0100

    basic UI

---
+++ MarcoSmiles/Assets/Own/Scripts/MS_LeapController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Leap;
+
+
+
+/// <summary>
+///     CLASS CREATED FOR MARCO_SMILES
+/// </summary>
+public class MS_LeapController : MonoBehaviour
+{
+    Controller controller;
+    MS_LeapListener listener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        controller = new Controller();
+
+        controller.Device += MS_LeapListener.OnLeapConnect;
+        //controller.FrameReady += MS_LeapListener.OnFrame;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (controller.IsServiceConnected)
+        {
+            Debug.Log("connesso");
+        }
+
+        if (controller.IsConnected)
+        {
+            Debug.Log("lollete");
+        }
+        */
+    }
+
+}
---
+++ MarcoSmiles/Assets/Own/Scripts/MS_LeapListener.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Leap;
+
+
+/// <summary>
+///     CLASS CREATED FOR MARCO_SMILES
+/// </summary>
+public class MS_LeapListener : MonoBehaviour
+{
+    /// <summary>
+    /// Evento attivato all'esecuzione, se il leap rileva le mani.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    public static void OnLeapConnect(object sender, DeviceEventArgs args)
+    {
+        //  stampa un messaggio di log
+        Debug.Log("Connesso");
+    }
+
+    /// <summary>
+    /// rileva frame
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    public static void OnFrame(object sender, FrameEventArgs args)
+    {
+        //Debug.Log("Frame disponibile");
+
+        Frame frame = args.frame;
+
+        Debug.Log($"" +
+            $"Frame id: {frame.Id}," +
+            $"timestamp: {frame.Timestamp}," +
+            $" hands: {frame.Hands.Count}");
+
+
+        //  per ogni mano, sceglie se è dx o sx, e stampa i seguenti dati
+        //  DATI GENERICI:          dx o sx, tupla di coords (x,y,z) dal sensore leap, numero dita,
+        //  ROTAZIONI relative:     rotazione su asse-x (hand pitch), rotazione su asse-z (hand roll), rotazione su asse-y (hand yaw)
+        foreach (var hand in frame.Hands)
+        {
+            var handType = hand.IsRight ? "Right Hand" : "Left Hand";
+
+            Debug.Log($"{handType}, Hand id: {hand.Id}, palm position: {hand.PalmPosition}, fingers: {hand.Fingers.Count}");
+
+            Vector normal = hand.PalmNormal;
+            Vector direction = hand.Direction;
+            Debug.Log($"Hand pitch: {direction.Pitch * 180.0f / (float)Mathf.PI} degrees," +
+                $" roll: {normal.Roll * 180.0f / (float)Mathf.PI} degrees," +
+                $" yaw: {direction.Yaw * 180.0f / (float)Mathf.PI} degrees");
+
+            // hand.Fingers[3].bones[2].Direction.Yaw
+
+
+        }
+    }
+}
