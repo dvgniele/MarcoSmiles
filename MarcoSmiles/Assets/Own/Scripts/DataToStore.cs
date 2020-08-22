23985a2df2de3384dadbd4439f53dcb381ffdde9
Daniele Perrella <ktm17@live.it>
2020-08-22 10:13:10 +0200

    piccolo pushino

---
+++ MarcoSmiles/Assets/Own/Scripts/DataToStore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class DataToStore
+{
+    public int PosX;
+
+    public DataToStore(int PosX)
+    {
+        this.PosX = PosX;
+    }
+}
---
+++ MarcoSmiles/Assets/Own/Scripts/FileUtils.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+using UnityEngine;
+
+public static class FileUtils
+{
+    static string path = Application.persistentDataPath;
+
+    public static void Save(DataToStore data)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(GeneratePath("marcosmiles_save"), FileMode.Create);
+
+        bf.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static DataToStore Load()
+    {
+        if (File.Exists(GeneratePath("marcosmiles_save")))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(GeneratePath("marcosmiles_save"), FileMode.Open);
+
+            DataToStore data = bf.Deserialize(stream) as DataToStore;
+
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("File non trovato");
+            return null;
+        }
+    }
+
+    private static string GeneratePath(string filename)
+    {
+        return $"{path}/{filename}.gepao";
+    }
+}
