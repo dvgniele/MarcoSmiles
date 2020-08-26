e86b96dca9a4dc909490b8de498cd7023e91ff50
Daniele Perrella <ktm17@live.it>
2020-08-26 18:25:07 +0200

    training scene

---
+++ MarcoSmiles/Assets/Own/Scripts/TrainingScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainingScript : MonoBehaviour
+{
+    public Text countDown_Text;
+    int count = 3;
+
+    bool counting_flag = false;
+
+
+    /*
+    
+        GEPPA LAVORA IN QUESTA
+
+    */
+    private void FUNZIONE_DI_GEPAO()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public void Trainer()
+    {
+        if(!counting_flag)
+        {
+            count = 4;
+
+            StartCoroutine(Waiter());
+            counting_flag = true;
+        }
+    }
+
+    IEnumerator Waiter()
+    {
+        if (count > 0)
+        {
+            count--;
+            countDown_Text.text = count.ToString();
+
+            yield return new WaitForSeconds(1);
+            StartCoroutine(Waiter());
+        }
+        else
+        {
+            counting_flag = false;
+
+            FUNZIONE_DI_GEPAO();
+        }
+
+    }
+}
